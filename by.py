70e5e4c58e3b2659f63d1affb7e9b1c9ca77d725
Behzod Lozoyev <231000372+BEHRUZBEK-7@users.noreply.github.com>
2025-10-31 15:57:39 +0500

    .

---
+++ by.py
@@ -0,0 +1,229 @@
+# #1-masala
+# fruits = ['olma', 'banan', 'uzum']
+
+# fruits.append('nok')
+
+# print(fruits)
+
+# print(len(fruits))
+
+# print('olma' in fruits)
+
+# print(fruits.index('banan'))
+
+
+#2-msala
+# colors = ['qizil', 'yashil', 'ko\'k']
+
+# colors.append('sariq')
+# print(colors)
+
+# colors.sort()
+# print(colors)
+
+# print(len(colors))
+
+# print('yashil' in colors)
+
+
+# #3-masala
+# cities = ['Toshkent', 'Samarqand', 'Buxoro']
+
+# cities.append('Xiva')
+# print(cities)
+
+# print(cities.index('Samarqand'))
+
+# print(len(cities))
+
+# cities.sort()
+# print(cities)
+
+# print('Buxoro' in cities)
+
+# #4-masala
+
+# numbers = [5, 2, 8, 1]
+# numbers.append(10)
+# print(numbers)  
+# numbers.sort()
+# print(numbers)  
+# print(len(numbers))
+# print(2 in numbers)
+# print(numbers.index(8))
+
+# #5-masala 
+# #   
+# animals = ['it', 'mushuk', 'quyon']
+# animals.append('sigir')
+# print(animals)
+# animals.sort()
+# print(animals)
+# print(len(animals))
+# print('mushuk' in animals)
+# print(animals.index('quyon'))
+
+# #6-masala
+
+# cars = ['Toyota', 'BMW', 'Audi']
+# cars.append('Mercedes')
+# print(cars)
+# cars.sort()
+# print(cars)
+# print(len(cars))
+# print('BMW' in cars)
+# print(cars.index('Audi'))
+
+# #7-masala
+
+# countries = ['O‘zbekiston', 'Qozog‘iston', 'Rossiya']
+# countries.append('Xitoy')
+# print(countries)
+# countries.sort()
+# print(countries)
+# print(len(countries))
+# print('Rossiya' in countries)
+# print(countries.index('Qozog‘iston'))
+
+# #8-masala
+
+# languages = ['Python', 'Java', 'C++']   
+# languages.append('JavaScript')
+# print(languages)
+# languages.sort()
+# print(languages)
+# print(len(languages))
+# print('Java' in languages)
+# print(languages.index('C++'))
+
+# #9-masala
+
+# cities = ['New York', 'Los Angeles', 'Chicago']
+# cities.append('Houston')
+# print(cities)
+# cities.sort()
+# print(cities)
+# print(len(cities))
+# print('Chicago' in cities)
+# print(cities.index('Los Angeles'))
+
+# #10-masala
+
+# planets = ['Earth', 'Mars', 'Jupiter']
+# planets.append('Saturn')
+# print(planets)
+# planets.sort()
+# print(planets)
+# print(len(planets))
+# print('Mars' in planets)
+# print(planets.index('Jupiter'))
+
+# #11-masala
+
+# days = ['Monday', 'Tuesday', 'Wednesday']
+# days.append('Thursday')
+# print(days)
+# days.sort()
+# print(days)
+# print(len(days))
+# print('Tuesday' in days)
+# print(days.index('Wednesday'))
+
+# #12-masala
+
+# seasons = ['Spring', 'Summer', 'Autumn']
+# seasons.append('Winter')
+# print(seasons)
+# seasons.sort()
+# print(seasons)
+# print(len(seasons))
+# print('Summer' in seasons)
+# print(seasons.index('Autumn'))
+
+# #13-masala
+
+# colors = ['Red', 'Green', 'Blue']
+# colors.append('Yellow')
+# print(colors)
+# colors.sort()
+# print(colors)
+# print(len(colors))
+# print('Green' in colors)
+# print(colors.index('Blue'))
+
+# #14-masala
+
+# shapes = ['Circle', 'Square', 'Triangle']
+# shapes.append('Rectangle')
+# print(shapes)
+# shapes.sort()
+# print(shapes)
+# print(len(shapes))
+# print('Square' in shapes)
+# print(shapes.index('Triangle'))
+
+# #15-masala
+
+# instruments = ['Guitar', 'Piano', 'Drums']
+# instruments.append('Violin')
+# print(instruments)
+# instruments.sort()
+# print(instruments)
+# print(len(instruments))
+# print('Piano' in instruments)
+# print(instruments.index('Drums'))
+
+# #16-masala
+
+# beverages = ['Tea', 'Coffee', 'Juice']
+# beverages.append('Water')
+# print(beverages)
+# beverages.sort()
+# print(beverages)
+# print(len(beverages))
+# print('Coffee' in beverages)
+# print(beverages.index('Juice'))
+
+# #17-masala
+
+# hobbies = ['Reading', 'Traveling', 'Cooking']
+# hobbies.append('Gardening')
+# print(hobbies)
+# hobbies.sort()
+# print(hobbies)
+# print(len(hobbies))
+# print('Traveling' in hobbies)
+# print(hobbies.index('Cooking'))
+
+# #18-masala
+
+# cart = [ 'non', 'sut', 'tuxum', "go'sht"]
+# cart.append('piyoz')
+# print(cart)
+# cart.sort()
+# print(cart)
+# print(len(cart))
+# print('sut' in cart)
+# print(cart.index("go'sht"))
+
+# # #19-masala
+# champions = ['Khamzat Chimaev', 'Khabib Nurmagomedov', 'Conor McGregor', 'Mike Tyson', 'Wladimir Klichko', 'Eliud']
+# champions.append('Usain Bolt')
+# print(champions)    
+# champions.sort()
+# print(champions)
+# print(len(champions))
+# print('Khabib Nurmagomedov' in champions)
+# print(champions.index('Conor McGregor'))
+
+# # #20-masala
+
+# cities = ['Toshkent', 'Samarqand', 'Buxoro', 'Andijon', 'Namangan']
+# cities.append('Fargona')
+# print(cities)
+# cities.sort()
+# print(cities)
+# print(len(cities))
+# print('Samarqand' in cities)
+# print(cities.index('Buxoro'))
+# tugadi
